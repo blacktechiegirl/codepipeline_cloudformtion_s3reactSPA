48e2d80cc000db6ab55d131d6961b1524dbd2e96
Aminat <amibarbs01@gmail.com>
2022-06-25 17:05:00 +0100

    installed react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Congratulations you made your first CI/CD Pipeline.</h1>
+      <h2>&#127755;</h2>
+    </div>
+  );
+}
+
+export default App;
